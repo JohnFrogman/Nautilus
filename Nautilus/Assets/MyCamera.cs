2b34ce602ae56bf2fc318af68ffe60edd3506f70
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-07-11 13:25:50 +0100

    Initial stuff
    Basic movement and camera

---
+++ Nautilus/Assets/MyCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCamera : MonoBehaviour
+{
+
+	private float x;
+	private float z;
+	public float zoom = 50f;
+	public GameObject focus;
+
+	// The camera is just fixed to the player.
+	// Update is called once per frame
+	void Update () 
+	{
+		x = focus.transform.position.x;
+		z = focus.transform.position.z;
+		gameObject.transform.position = new Vector3 (x, zoom, z);
+	}
+}
---
+++ Nautilus/Assets/PlayerControl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour 
+{
+
+	public float maxSpeed = 50; 
+	public float acceleration = 2;
+	public float turnRate = 3;
+	// Coefficient that controls the speed of the break, this must always be less than 1
+	// If it's too low then the player will struggle to come to a halt at all, too high and it
+	// feels silly. Above one and it bugs out.
+	public float brakepower = 0.1F;
+	public float speed;
+	
+	void FixedUpdate () 
+	{
+		// Rotates the vehicle around, it's forward and back rather than left and right for the rotation but w/e.
+		if (Input.GetButton("Turn")) 
+		{
+			gameObject.transform.Rotate(Input.GetAxisRaw("Turn")*turnRate*Vector3.up);
+		}
+		if (Input.GetButton ("Accelerate")) 
+		{
+			// Stops you breaking the speed limit, for both forward and reverse.
+			if (speed*Input.GetAxisRaw("Accelerate") < maxSpeed)
+			{
+				speed += Input.GetAxisRaw("Accelerate")*acceleration;
+			}
+		}
+		// Exponentially decaying function
+		if (Input.GetButton ("Brake")) 
+		{
+			if (speed < 1)
+			{
+				speed = 0;
+			}
+			else
+			{
+				speed -= brakepower*speed;
+			}
+		}
+		
+		// The current speed.
+		gameObject.transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
+	}
+}
